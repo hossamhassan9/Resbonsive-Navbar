b55477824a85509d9722333e208cab2f2c09731f
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2019-12-05 15:22:47 +0200

    Adding the content&&Snooth scroll

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const navbarToggler = document.querySelector('.navbar-toggler');
-const navbarMenu = document.querySelector('.navbar ul');
+const navbarToggler = document.querySelector('.navbar-toggler')
+const navbarMenu = document.querySelector('.navbar ul')
+const navbarLinks = document.querySelectorAll('.navbar a')
 
-navbarToggler.addEventListener('click', navbarTogglerClick);
+
+navbarToggler.addEventListener('click', navbarTogglerClick)
 
 function navbarTogglerClick() {
-  navbarToggler.classList.toggle('open-navbar-toggler');
-  navbarMenu.classList.toggle('open');
+  navbarToggler.classList.toggle('open-navbar-toggler')
+  navbarMenu.classList.toggle('open')
+}
+
+navbarLinks.forEach(e => e.addEventListener('click', navbarLinkClick))
+
+function navbarLinkClick() {
+  if (navbarMenu.classList.contains('open')) {
+    navbarToggler.click()
+  }
 }
